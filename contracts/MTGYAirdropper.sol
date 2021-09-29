793cca3277f74e6463d8e2002730f768e1816e62
Lance Whatley <whatl3y@gmail.com>
2021-09-29 07:06:23 -0400

    Use interfaces

--- contracts/MTGYAirdropper.sol
+++ contracts/MTGYAirdropper.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.4;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/interfaces/IERC20.sol';
+import '@openzeppelin/contracts/interfaces/IERC721.sol';
 import './MTGYSpend.sol';
 
 /**
  * @title MTGYAirdropper
- * @dev Allows sending an ERC20 token to multiple addresses in different amounts
+ * @dev Allows sending ERC20 or ERC721 tokens to multiple addresses
  */
 contract MTGYAirdropper is Ownable {
-  ERC20 private _mtgy;
+  IERC20 private _mtgy;
   MTGYSpend private _mtgySpend;
 
   address public mtgyTokenAddy;
@@ -26,13 +26,13 @@ contract MTGYAirdropper is Ownable {
   constructor(address _mtgyTokenAddy, address _mtgySpendAddy) {
     mtgyTokenAddy = _mtgyTokenAddy;
     mtgySpendAddy = _mtgySpendAddy;
-    _mtgy = ERC20(_mtgyTokenAddy);
+    _mtgy = IERC20(_mtgyTokenAddy);
     _mtgySpend = MTGYSpend(_mtgySpendAddy);
   }
 
   function changeMtgyTokenAddy(address _tokenAddy) external onlyOwner {
     mtgyTokenAddy = _tokenAddy;
-    _mtgy = ERC20(_tokenAddy);
+    _mtgy = IERC20(_tokenAddy);
   }
 
   function changeMtgySpendAddy(address _spendAddy) external onlyOwner {
@@ -69,7 +69,7 @@ contract MTGYAirdropper is Ownable {
   ) external returns (bool) {
     _payForService();
 
-    ERC20 _token = ERC20(_tokenAddress);
+    IERC20 _token = IERC20(_tokenAddress);
     for (uint256 _i = 0; _i < _addressesAndAmounts.length; _i++) {
       Receiver memory _user = _addressesAndAmounts[_i];
       _token.transferFrom(msg.sender, _user.userAddress, _user.amountOrTokenId);
@@ -83,7 +83,7 @@ contract MTGYAirdropper is Ownable {
   ) external returns (bool) {
     _payForService();
 
-    ERC721 _token = ERC721(_tokenAddress);
+    IERC721 _token = IERC721(_tokenAddress);
     for (uint256 _i = 0; _i < _addressesAndAmounts.length; _i++) {
       Receiver memory _user = _addressesAndAmounts[_i];
       _token.transferFrom(msg.sender, _user.userAddress, _user.amountOrTokenId);
