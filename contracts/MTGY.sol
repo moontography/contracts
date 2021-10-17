63c8f6190b07f6dda96ff16a14476667331bd2e5
Lance Whatley <whatl3y@gmail.com>
2021-10-17 13:08:01 -0400

    Remove MTGY spend

--- contracts/MTGY.sol
+++ contracts/MTGY.sol
@@ -9,13 +9,6 @@ contract MTGY is IERC20 {
   string public constant symbol = 'MTGY';
   uint8 public constant decimals = 18;
 
-  address public constant burnWallet =
-    0x000000000000000000000000000000000000dEaD;
-  address public constant devWallet =
-    0x3A3ffF4dcFCB7a36dADc40521e575380485FA5B8;
-  address public constant rewardsWallet =
-    0x87644cB97C1e2Cc676f278C88D0c4d56aC17e838;
-
   mapping(address => uint256) balances;
   mapping(address => mapping(address => uint256)) allowed;
 
@@ -86,29 +79,6 @@ contract MTGY is IERC20 {
     allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
     balances[buyer] = balances[buyer].add(numTokens);
     emit Transfer(owner, buyer, numTokens);
-    return true;
-  }
-
-  /**
-   * spendOnProduct: used by a moontography product for a user to spend their tokens on usage of a product
-   *   25% goes to dev wallet
-   *   25% goes to rewards wallet for rewards
-   *   50% burned
-   */
-  function spendOnProduct(uint256 amountTokens) public returns (bool) {
-    require(amountTokens <= balances[msg.sender]);
-    balances[msg.sender] = balances[msg.sender].sub(amountTokens);
-    uint256 half = amountTokens / 2;
-    uint256 quarter = half / 2;
-    // 50% burn
-    balances[burnWallet] = balances[burnWallet].add(half);
-    // 25% rewards wallet
-    balances[rewardsWallet] = balances[rewardsWallet].add(quarter);
-    // 25% dev wallet
-    balances[devWallet] = balances[devWallet].add(
-      amountTokens - half - quarter
-    );
-    emit Spend(msg.sender, amountTokens);
     return true;
   }
 }
